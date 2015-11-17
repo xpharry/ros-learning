93eaf39fe739865e4f1f3e05802028ba687d048a
xpharry <xpharry123@hotmail.com>
2015-11-16 22:15:04 -0500

    reference update 11-16

--- Reference/cwru_baxter/cwru_pcl_utils/include/cwru_pcl_utils/cwru_pcl_utils.h
+++ Reference/cwru_baxter/cwru_pcl_utils/include/cwru_pcl_utils/cwru_pcl_utils.h
@@ -22,6 +22,8 @@
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/TransformStamped.h>
 
+#include <cwru_msgs/PatchParams.h>
+
 #include <tf/transform_listener.h>  // transform listener headers
 #include <tf/transform_broadcaster.h>
 
@@ -58,6 +60,8 @@ public:
     void fit_points_to_plane(Eigen::MatrixXf points_array, 
         Eigen::Vector3f &plane_normal, 
         double &plane_dist); 
+    Eigen::Vector3f compute_centroid(pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_ptr);
+    
     void fit_points_to_plane(pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_ptr,Eigen::Vector3f &plane_normal, double &plane_dist);
     void fit_xformed_selected_pts_to_plane(Eigen::Vector3f &plane_normal, double &plane_dist);  
 
@@ -81,31 +85,74 @@ public:
     bool got_kinect_cloud() { return got_kinect_cloud_; };
     bool got_selected_points() {return got_selected_points_;};
     void save_kinect_snapshot() {    pcl::io::savePCDFileASCII ("kinect_snapshot.pcd", *pclKinect_ptr_);};
+    //alternative "save" fnc: save as a colored pointcloud
+    void save_kinect_clr_snapshot() {pcl::io::savePCDFileASCII ("kinect_clr_snapshot.pcd", *pclKinect_clr_ptr_);};
     void save_transformed_kinect_snapshot() { pcl::io::savePCDFileASCII ("xformed_kinect_snapshot.pcd", *pclTransformed_ptr_);};
+    void get_transformed_selected_points(pcl::PointCloud<pcl::PointXYZ> & outputCloud );
+    void copy_cloud(PointCloud<pcl::PointXYZ>::Ptr inputCloud, PointCloud<pcl::PointXYZ>::Ptr outputCloud); 
+    void copy_cloud_xyzrgb_indices(PointCloud<pcl::PointXYZRGB>::Ptr inputCloud, vector<int> &indices, PointCloud<pcl::PointXYZRGB>::Ptr outputCloud);
 
+    void get_indices(vector<int> &indices) {   indices = indices_;};
+    //same as above, but assumes 
+    void copy_indexed_pts_to_output_cloud(vector<int> &indices,PointCloud<pcl::PointXYZRGB> &outputCloud);
+
+    void get_gen_purpose_cloud(pcl::PointCloud<pcl::PointXYZ> & outputCloud );    
+    void example_pcl_operation();
+    //operate on transformed Kinect data and identify point indices within +/-z_eps of specified height
+    void filter_cloud_z(PointCloud<pcl::PointXYZ>::Ptr inputCloud, double z_nom, double z_eps, vector<int> &indices);
+    // as above, specifically for transformed kinect cloud:
+    void find_coplanar_pts_z_height(double plane_height,double z_eps,vector<int> &indices);
+    // find pts within +/- z_eps of z_height, AND within "radius" of "centroid"
+    void filter_cloud_z(PointCloud<pcl::PointXYZ>::Ptr inputCloud, double z_nom, double z_eps, 
+                double radius, Eigen::Vector3f centroid, vector<int> &indices);
+    //same as above, but specifically operates on transformed kinect cloud
+    void filter_cloud_z(double z_nom, double z_eps, 
+                double radius, Eigen::Vector3f centroid, vector<int> &indices);   
+    
+    void analyze_selected_points_color();
+    
+    Eigen::Vector3f get_centroid() { return centroid_; };
+    Eigen::Vector3f get_major_axis() { return major_axis_; };
+    Eigen::Vector3d find_avg_color();
+    Eigen::Vector3d find_avg_color_selected_pts(vector<int> &indices);
+    void find_indices_color_match(vector<int> &input_indices,
+                    Eigen::Vector3d normalized_avg_color,
+                    double color_match_thresh, vector<int> &output_indices);    
+    
 private:
     ros::NodeHandle nh_; 
     // some objects to support subscriber, service, and publisher
     ros::Subscriber pointcloud_subscriber_; //use this to subscribe to a pointcloud topic
     ros::Subscriber selected_points_subscriber_; // this to subscribe to "selectedPoints" topic from Rviz
+
     
     //ros::ServiceServer minimal_service_; //maybe want these later
-    //ros::Publisher  minimal_publisher_;
+    ros::Publisher  pointcloud_publisher_;
+    ros::Publisher patch_publisher_;    
+    
+    
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclKinect_clr_ptr_; //pointer for color version of pointcloud
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr pclSelectedPtsClr_ptr_; //pointer for color version of pointcloud
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr pclKinect_ptr_; //(new PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr pclTransformed_ptr_;
     pcl::PointCloud<pcl::PointXYZ>::Ptr pclSelectedPoints_ptr_;
     pcl::PointCloud<pcl::PointXYZ>::Ptr pclTransformedSelectedPoints_ptr_;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pclGenPurposeCloud_ptr_;
     
     bool got_kinect_cloud_;
     bool got_selected_points_;
     // member methods as well:
     void initializeSubscribers(); // we will define some helper methods to encapsulate the gory details of initializing subscribers, publishers and services
-    //void initializePublishers();
+    void initializePublishers();
     //void initializeServices();
     
     void kinectCB(const sensor_msgs::PointCloud2ConstPtr& cloud); //prototype for callback fnc
-    void selectCB(const sensor_msgs::PointCloud2ConstPtr& cloud); // callback for selected points    
+    void selectCB(const sensor_msgs::PointCloud2ConstPtr& cloud); // callback for selected points  
+    
+    Eigen::Vector3f major_axis_,centroid_; 
+    Eigen::Vector3d avg_color_;
+    vector<int> indices_; // put interesting indices here
     //prototype for example service
     //bool serviceCallback(example_srv::simple_bool_service_messageRequest& request, example_srv::simple_bool_service_messageResponse& response);
 
